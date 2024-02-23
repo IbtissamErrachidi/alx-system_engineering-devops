7fd8ae78cbbb29fb2f2ad21607ba69b5acb26925
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-23 22:45:53 +0000

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,9 @@ package {'flask':
   ensure => '2.1.0',
 provider => 'pip3'
 }
-package {'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3'
+#!/usr/bin/pup
+#Installing of flask v(2.1.0)
+package {'flask':
+  ensure => '2.1.1',
+provider => 'pip3'
 }
