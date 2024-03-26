c144895d709e08b4999e34e70e3414e08907cfd9
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-26 01:21:02 +0000

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,13 +12,27 @@ def fetch_user_data():
     url = "https://jsonplaceholder.typicode.com/"
 
     users = requests.get(url + "users").json()
+    data_to_export = {}
+    for user in users:
+        user_id = user["id"]
+        user_url = url + f"todos?userId={user_id}"
+        todo_list = requests.get(user_url).json()
+
+        data_to_export[user_id] = [
+            {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username"),
+            }
+            for todo in todo_list
+        ]
+
+    return data_to_export
+
+
+if __name__ == "__main__":
+    data_to_export = fetch_user_data()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+        json.dump(data_to_export, jsonfile, indent=4)
+
