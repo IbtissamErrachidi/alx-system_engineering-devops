f619ba51389f791355c95296c85affce31511ac5
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-26 01:11:54 +0000

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,13 +12,23 @@ def fetch_user_data():
     url = "https://jsonplaceholder.typicode.com/"
 
     users = requests.get(url + "users").json()
-
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+        all_tasks = {}
+        for user in users:
+            user_id = user.get("id")
+            tasks = requests.get(url + "todos", params={"userId": user_id}).json()
+            user_tasks = [{
+                "userId": user_id,  # Ajout du champ userId
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get("username")
+            } for task in tasks]
+            all_tasks[user_id] = user_tasks
+        
+        # Vérification de la présence de tous les utilisateurs
+        all_users_exist = all(user_id in all_tasks for user_id in range(1, len(users) + 1))
+        json.dump(all_tasks, jsonfile)
+    
+    return all_users_exist
+
+
