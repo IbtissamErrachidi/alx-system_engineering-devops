26b03101093ea530bb5a91feb5601c393e3b1245
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-26 00:45:09 +0000

    CSV_file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""with employee ID export a CSV format ."""
+"""
+with employee ID export a CSV format .
+"""
 
 import csv
 import requests
@@ -8,11 +10,9 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-
     url = "https://jsonplaceholder.typicode.com/"
 
     user = requests.get(url + "users/{}".format(user_id)).json()
-
     username = user.get("username")
 
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
