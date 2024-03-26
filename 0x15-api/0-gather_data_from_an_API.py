0eb6556cb3cea981834758142117309fc9701ef3
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-26 00:03:53 +0000

    data_from_an_api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,19 +8,17 @@ import sys
 
 
 if __name__ == "__main__":
-    # URL de base pour l'API JSONPlaceholder
-    url = "https://jsonplaceholder.typicode.com/"
+	# URL de base pour l'API JSONPlaceholder
+	url = "https://jsonplaceholder.typicode.com/"
 
-    employee_id = sys.argv[1]
+	employee_id = sys.argv[1]
 
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+	user = requests.get(url + "users/{}".format(employee_id)).json()
+	params = {"userId": employee_id}
+	todos = requests.get(url + "todos", params).json()
+	completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-
-    print("Employee {} is done with tasks({}/{}):".format(name, todos_done, todos_count).strip())
-
-    [print("\t {}".format(complete)) for complete in completed]
+	print("Employé {} est à jour avec les tâches({}/{}):".format(
+	user.get("name"), len(completed), len(todos)))
+	[print("\t {}".format(complete)) for complete in completed]
 
